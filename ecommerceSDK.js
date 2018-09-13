43f2e3f6df5688fe597f411536ebeb0c55bc840e
FacuRiccardi <riccardifacu@gmail.com>
2018-09-13 19:50:55 -0300

    Fixed asynchronism problems
    A callback parameter is added, because the return does not work due to the asynchronism of the functions

--- ecommerceSDK.js
+++ ecommerceSDK.js
@@ -2,59 +2,63 @@
 
 var ecommerce = (function(){
     const url = 'http://ecommerce.casu-net.com.ar/api'
-
+    var token;
+    
     return {
+
+        init: function(token){
+            this.token = token;
+        },
         
-        registerUser: function(email, password, firstname, lastname){
+        registerUser: function(email, password, firstname, lastname, callback){
             $.ajax({
                 method: 'POST',
                 url: url + '/users',
-                dataType: 'json',
                 data: {email: email, password: password, firstname: firstname, lastname: lastname}
             }).done(function(body){
-                return body
+                callback(body);
             })
         },
 
-        authenticateUser: function(email, password){
+        authenticateUser: function(email, password, callback){
+            var contexto = this;
             $.ajax({
                 method: 'POST',
                 url: url + '/users/authenticate',
-                dataType: 'json',
                 data: {email: email, password: password}
             }).done(function(body){
-                console.log(body)
-                localStorage.setItem('token', body.token)
+                contexto.token = body.token;
+                callback(body);
             })
         },
 
-        getProducts: function(){
+        getProducts: function(callback){
             $.ajax({
                 method: 'GET',
                 url: url + '/products'
             }).done(function(body){
-                return body
+                callback(body);
             })
         },
 
-        getCart: function(){
+        getCart: function(callback){
             $.ajax({
                 method: 'GET',
                 url : url + '/cart',
                 headers: {
-                    'x-access-token': localStorage.getItem('token')
+                    'x-access-token': this.token
                 }
             }).done(function(body){
-                return body
+                callback(body);
             })
         },
 
-        addProductToCart: function(productId, qty){
+        addProductToCart: function(productId, qty, callback){
             $.ajax({
                 method: 'POST',
                 url : url + '/cart',
                 headers: {
-                    'x-access-token': localStorage.getItem('token'),
+                    'x-access-token': this.token,
                     'Content-type': 'application/json'
                 },
                 data: JSON.stringify({
@@ -62,58 +66,58 @@ var ecommerce = (function(){
                     'qty': parseInt(qty)
                 })
             }).done(function(body){
-                return body
+                callback(body);
             })
         },
 
-        deleteProductFromCart: function(productId, all){
+        deleteProductFromCart: function(productId, all, callback){
             $.ajax({
                 method: 'DELETE',
                 url : url + '/cart/' + productId + ((all != null)? '?all=1': ''),
                 headers: {
-                    'x-access-token': localStorage.getItem('token')
+                    'x-access-token': this.token
                 }
             }).done(function(body){
-                return body
+                callback(body);
             })
         },
         
-        getWishList: function(){
+        getWishList: function(callback){
             $.ajax({
                 method: 'GET',
                 url: url + '/whishlist',
                 headers:{
-                    'x-access-token': localStorage.getItem('token')
+                    'x-access-token': this.token
                 }
             }).done(function(body){
-                return body;
+                callback(body);
             })
         },
         
-        addItemToWishList: function(productId){
+        addItemToWishList: function(productId, callback){
             $.ajax({
                 method: 'POST',
                 url: url + '/whishlist',
                 headers: {
-                    'x-access-token': localStorage.getItem('token')
+                    'x-access-token': this.token
                 },
                 data: {
                     'productId': productId
                 }
             }).done(function(body){
-                return body
+                callback(body);
             })
         },
         
-        deleteItemFromWishList: function(productId){
+        deleteItemFromWishList: function(productId, callback){
             $.ajax({
                 method: 'DELETE',
                 url: url + '/whishlist/' + productId,
                 headers: {
-                    'x-access-token': localStorage.getItem('token')
+                    'x-access-token': this.token
                 }
             }).done(function(body){
-                return body
+                callback(body);
             })
         }
     }
