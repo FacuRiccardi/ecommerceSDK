2fd0debcf2e8dd12220d627b7f87374dbf86a76d
FacuRiccardi <riccardifacu@gmail.com>
2018-09-13 22:43:31 -0300

    application of the apiCall function
    Because a request is made repeatedly with the same data, everything is simplified in a private function

--- ecommerceSDK.js
+++ ecommerceSDK.js
@@ -3,122 +3,86 @@
 var ecommerce = (function(){
     const url = 'http://ecommerce.casu-net.com.ar/api'
     var token;
+
+    var apiCall = function(method, resource, headers, data, callback, skipAuth){
+        var h = headers || {}
+        if(!skipAuth){
+            h['x-access-token'] = token;
+        }
+        $.ajax({
+            method: method,
+            url: url + resource,
+            headers: h,
+            data: data
+        }).done(function(body){
+            callback(body);
+        }).error(function(err){
+            callback(err.responseJSON);
+        })
+    };
     
     return {
 
-        init: function(token){
-            this.token = token;
+        init: function(t){
+            token = t;
         },
         
         registerUser: function(email, password, firstname, lastname, callback){
-            $.ajax({
-                method: 'POST',
-                url: url + '/users',
-                data: {email: email, password: password, firstname: firstname, lastname: lastname}
-            }).done(function(body){
-                callback(body);
-            })
+            var data = {email: email, password: password, firstname: firstname, lastname: lastname}
+            apiCall('POST', '/users', null, data, callback, true)
         },
 
-        authenticateUser: function(email, password, callback){
-            var contexto = this;
+        authenticateUser: function(email, password, callback){            
             $.ajax({
                 method: 'POST',
                 url: url + '/users/authenticate',
                 data: {email: email, password: password}
             }).done(function(body){
-                contexto.token = body.token;
+                token = body.token;
                 callback(body);
+            }).error(function(err){
+                callback(err.responseJSON);
             })
         },
 
         getProducts: function(callback){
-            $.ajax({
-                method: 'GET',
-                url: url + '/products'
-            }).done(function(body){
-                callback(body);
-            })
+            apiCall('GET', '/products', null, null, callback, true)
         },
 
         getCart: function(callback){
-            $.ajax({
-                method: 'GET',
-                url : url + '/cart',
-                headers: {
-                    'x-access-token': this.token
-                }
-            }).done(function(body){
-                callback(body);
-            })
+            apiCall('GET', '/cart', null, null, callback);
         },
 
         addProductToCart: function(productId, qty, callback){
-            $.ajax({
-                method: 'POST',
-                url : url + '/cart',
-                headers: {
-                    'x-access-token': this.token,
+            var headers = {
                     'Content-type': 'application/json'
                 },
-                data: JSON.stringify({
+                data = JSON.stringify({
                     'productId': productId,
                     'qty': parseInt(qty)
-                })
-            }).done(function(body){
-                callback(body);
-            })
+                });
+            apiCall('POST', '/cart', headers, data, callback);
         },
 
-        deleteProductFromCart: function(productId, all, callback){
-            $.ajax({
-                method: 'DELETE',
-                url : url + '/cart/' + productId + ((all != null)? '?all=1': ''),
-                headers: {
-                    'x-access-token': this.token
-                }
-            }).done(function(body){
-                callback(body);
-            })
+        deleteProductFromCart: function(productId, callback, all){
+            var urlAux = '/cart/' + productId + ((all != null)? '?all=1': '');
+            apiCall('DELETE', urlAux, null, null, callback);
         },
         
         getWishList: function(callback){
-            $.ajax({
-                method: 'GET',
-                url: url + '/whishlist',
-                headers:{
-                    'x-access-token': this.token
-                }
-            }).done(function(body){
-                callback(body);
-            })
+            apiCall('GET', '/whishlist', null, null, callback);
         },
         
         addItemToWishList: function(productId, callback){
-            $.ajax({
-                method: 'POST',
-                url: url + '/whishlist',
-                headers: {
-                    'x-access-token': this.token
-                },
-                data: {
-                    'productId': productId
+            var data = {
+                    productId: productId
                 }
-            }).done(function(body){
-                callback(body);
-            })
+            apiCall('POST', '/whishlist', null, data, callback);
         },
         
         deleteItemFromWishList: function(productId, callback){
-            $.ajax({
-                method: 'DELETE',
-                url: url + '/whishlist/' + productId,
-                headers: {
-                    'x-access-token': this.token
-                }
-            }).done(function(body){
-                callback(body);
-            })
+            var urlAux = '/whishlist/' + productId;
+            apiCall('DELETE', urlAux, null, null, callback);
         }
     }
 })();
