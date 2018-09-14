b4cff419c33d272083336b6d24177d414d1478ba
FacuRiccardi <riccardifacu@gmail.com>
2018-09-14 09:54:37 -0300

    apiCall implementation in authentication
    added the implementation of apiCall in the authentication function

--- ecommerceSDK.js
+++ ecommerceSDK.js
@@ -1,88 +1,94 @@
 //ecommerce SDK
+var ecommerce = (function() {
+	const url = 'http://ecommerce.casu-net.com.ar/api'
+	var token;
 
-var ecommerce = (function(){
-    const url = 'http://ecommerce.casu-net.com.ar/api'
-    var token;
+	var apiCall = function(method, resource, headers, data, callback, skipAuth, done) {
+		var h = headers || {},
+			d = done || function(b) {
+				callback(b)
+			};
+		if (!skipAuth) {
+			h['x-access-token'] = token;
+		}
+		$.ajax({
+			method: method,
+			url: url + resource,
+			headers: h,
+			data: data
+		}).done(function(body) {
+			d(body);
+		}).error(function(err) {
+			callback(err.responseJSON);
+		});
+	};
 
-    var apiCall = function(method, resource, headers, data, callback, skipAuth){
-        var h = headers || {}
-        if(!skipAuth){
-            h['x-access-token'] = token;
-        }
-        $.ajax({
-            method: method,
-            url: url + resource,
-            headers: h,
-            data: data
-        }).done(function(body){
-            callback(body);
-        }).error(function(err){
-            callback(err.responseJSON);
-        })
-    };
-    
-    return {
+	return {
 
-        init: function(t){
-            token = t;
-        },
-        
-        registerUser: function(email, password, firstname, lastname, callback){
-            var data = {email: email, password: password, firstname: firstname, lastname: lastname}
-            apiCall('POST', '/users', null, data, callback, true)
-        },
+		init: function(t) {
+			token = t;
+		},
 
-        authenticateUser: function(email, password, callback){            
-            $.ajax({
-                method: 'POST',
-                url: url + '/users/authenticate',
-                data: {email: email, password: password}
-            }).done(function(body){
-                token = body.token;
-                callback(body);
-            }).error(function(err){
-                callback(err.responseJSON);
-            })
-        },
+		registerUser: function(email, password, firstname, lastname, callback) {
+			var data = {
+				email: email,
+				password: password,
+				firstname: firstname,
+				lastname: lastname
+			};
+			apiCall('POST', '/users', null, data, callback, true);
+		},
 
-        getProducts: function(callback){
-            apiCall('GET', '/products', null, null, callback, true)
-        },
+		authenticateUser: function(email, password, callback) {
+			var data = {
+					email: email,
+					password: password
+				},
+				done = function(b) {
+					token = b.token;
+					callback(b)
+				};
+			apiCall('POST', '/users/authenticate', null, data, callback, true, done);
+		},
 
-        getCart: function(callback){
-            apiCall('GET', '/cart', null, null, callback);
-        },
+		getProducts: function(callback) {
+			apiCall('GET', '/products', null, null, callback, true);
+		},
 
-        addProductToCart: function(productId, qty, callback){
-            var headers = {
-                    'Content-type': 'application/json'
-                },
-                data = JSON.stringify({
-                    'productId': productId,
-                    'qty': parseInt(qty)
-                });
-            apiCall('POST', '/cart', headers, data, callback);
-        },
+		getCart: function(callback) {
+			apiCall('GET', '/cart', null, null, callback);
+		},
 
-        deleteProductFromCart: function(productId, callback, all){
-            var urlAux = '/cart/' + productId + ((all != null)? '?all=1': '');
-            apiCall('DELETE', urlAux, null, null, callback);
-        },
-        
-        getWishList: function(callback){
-            apiCall('GET', '/whishlist', null, null, callback);
-        },
-        
-        addItemToWishList: function(productId, callback){
-            var data = {
-                    productId: productId
-                }
-            apiCall('POST', '/whishlist', null, data, callback);
-        },
-        
-        deleteItemFromWishList: function(productId, callback){
-            var urlAux = '/whishlist/' + productId;
-            apiCall('DELETE', urlAux, null, null, callback);
-        }
-    }
+		addProductToCart: function(productId, qty, callback) {
+			var headers = {
+					'Content-type': 'application/json'
+				},
+				data = JSON.stringify({
+					'productId': productId,
+					'qty': parseInt(qty)
+				});
+			apiCall('POST', '/cart', headers, data, callback);
+		},
+
+		deleteProductFromCart: function(productId, callback, all) {
+			var urlAux = '/cart/' + productId + ((all != null) ? '?all=1' : '');
+			apiCall('DELETE', urlAux, null, null, callback);
+		},
+
+		getWishList: function(callback) {
+			apiCall('GET', '/whishlist', null, null, callback);
+		},
+
+		addItemToWishList: function(productId, callback) {
+			var data = {
+				productId: productId
+			};
+			apiCall('POST', '/whishlist', null, data, callback);
+		},
+
+		deleteItemFromWishList: function(productId, callback) {
+			var urlAux = '/whishlist/' + productId;
+			apiCall('DELETE', urlAux, null, null, callback);
+		}
+	}
 })();
